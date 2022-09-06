cff02a3a00955989a8091d351499043de83c6a74
Manion-sebastian <Sjmanion123@gmail.com>
2022-09-06 12:28:06 -0700

    updates

--- index.js
+++ index.js
@@ -20,23 +20,33 @@ app.get('/', (req, res) => {
 
 })
 
+const readDinoFile = () => {
+    const dinosaurs = fs.readFileSync('./dinosaurs.json')
+    const dinoData = JSON.parse(dinosaurs)
+    return dinoData
+}
+
 app.get('/dinosaurs', (req, res) => {
     // use fs to read the dino.json 
     // parse the file into json data
     // send the dino info to the client. 
-    const dinosaurs = fs.readFileSync('./dinosaurs.json')
-    const dinoData = JSON.parse(dinosaurs)
-    console.log(dinoData)
-    res.send('show all dinos')
+    // read file
+    const dinoData = readDinoFile()
+    // res.send('show all dinos')
+    res.json(dinoData)
 })
 app.get('/dinosaurs/new', (req, res) => {
     res.send('show a form to create a new dino')
 })
 
 app.post('/dinosaurs', (req, res) => {
+    // do not render a template. 
+    // redirect to where you can find a template.
     res.send('creates a new dino in the db')
 })
 
 app.get('/dinosaurs/:id', (req, res) => {
-    res.send(`show deatails for dino with id of ${req.params.id}`)
+    const dinoData = readDinoFile()
+    const dino = dinoData[req.params.id]
+    res.json(dino)
 })
