72d88efa3579990360974519239f2e19def4809d
Manion-sebastian <Sjmanion123@gmail.com>
2022-09-06 19:45:55 -0700

    working on implementing controllers

--- index.js
+++ index.js
@@ -24,38 +24,9 @@ app.get('/', (req, res) => {
 
 })
 
-const readDinoFile = () => {
-    const dinosaurs = fs.readFileSync('./dinosaurs.json')
-    const dinoData = JSON.parse(dinosaurs)
-    return dinoData
-}
 
-app.get('/dinosaurs', (req, res) => {
-    // use fs to read the dino.json 
-    // parse the file into json data
-    // send the dino info to the client. 
-    // read file
-    const dinoData = readDinoFile()
-    // res.send('show all dinos')
-    res.render('dinos/index', {dinos: dinoData})
-})
-app.get('/dinosaurs/new', (req, res) => {
-    res.render('dinos/new')
-})
 
-app.post('/dinosaurs', (req, res) => {
-    // do not render a template. 
-    // redirect to where you can find a template.
-    const dinoData = readDinoFile()
-    console.log(req.body)
-    dinoData.push(req.body)
-    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
 
-    res.redirect('/dinosaurs')
-})
 
-app.get('/dinosaurs/:id', (req, res) => {
-    const dinoData = readDinoFile()
-    const dino = dinoData[req.params.id]
-    res.json(dino)
-})
+
+
